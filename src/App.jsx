f52868410f94e0d9887e95743271e26f5c241f84
pibelanzallamas <brandoncastillo.09@gmail.com>
2025-02-20 00:18:24 -0300

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import { useState } from "react";
-import "./App.css";
+import "./styles/app.css";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Works from "./components/Works";
+import Fees from "./components/Fees";
+import Contact from "./components/Contact";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/works" element={<Works />} />
+        <Route path="/fees" element={<Fees />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
