876e0e4bcdaf33c61ae2a3a0de9d1d11332cd6aa
pibelanzallamas <brandoncastillo.09@gmail.com>
2025-02-22 14:56:09 -0300

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Contact() {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const [msg, setMsg] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const obj = {
+      name,
+      number,
+      msg,
+    };
+    console.log(obj);
+  };
+
   return (
-    <main className="all-contact">
+    <main className="contact">
       <h3>Contact</h3>
+      <div className="contact-info">
+        <p>
+          <span>📲</span> 1139241058
+        </p>
+        <p>
+          <span>📩</span> brancastillo.developer@icloud.com
+        </p>
+        <p>
+          <span>📍</span> Buenos Aires, Argentina
+        </p>
+      </div>
+      <hr />
+      <form onSubmit={(e) => handleSubmit(e)} className="contact-form">
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Nombre"
+        />
+        <input
+          vale={number}
+          onChange={(e) => setNumber(e.target.value)}
+          type="text"
+          placeholder="Número"
+        />
+        <textarea
+          value={msg}
+          onChange={(e) => setMsg(e.target.value)}
+          type="text"
+          placeholder="Presupuesto Gratis!"
+          rows={8}
+        />
+        <button type="submit">Enviar!</button>
+      </form>
     </main>
   );
 }
